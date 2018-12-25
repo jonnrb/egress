7c436e813b5998f9fb503beb1c0bcdc3f2f5284b
Jon Betti <jonbetti@gmail.com>
2018-12-25 04:40:57 +0000

    Split the forward rule

--- fw/apply.go
+++ fw/apply.go
@@ -12,7 +12,7 @@ func Apply(cfg Config) error {
 			Apply(rules.BaseRules).
 			Apply(addFlatNetworkForwarding(cfg)).
 			Add(50, []rules.Rule{
-				forward(cfg.LAN(), cfg.Uplink(), ""),
+				forward(cfg.LAN(), cfg.Uplink()),
 				masquerade(cfg.Uplink()),
 			}).
 			Add(60, cfg.ExtraRules()).
@@ -22,7 +22,7 @@ func Apply(cfg Config) error {
 func addFlatNetworkForwarding(cfg Config) func(rb rules.RuleSetBuilder) {
 	var rs rules.RuleSet
 	for _, s := range cfg.FlatNetworks() {
-		rs = append(rs, forward(cfg.LAN(), s.Link, s.Subnet))
+		rs = append(rs, forwardToSubnet(cfg.LAN(), s.Link, s.Subnet))
 	}
 
 	return func(rb rules.RuleSetBuilder) {
@@ -30,16 +30,16 @@ func addFlatNetworkForwarding(cfg Config) func(rb rules.RuleSetBuilder) {
 	}
 }
 
-func forward(a, b Link, dst string) rules.Rule {
-	if dst == "" {
-		return rules.Rule(fmt.Sprintf(
-			"-t filter -A fw-interfaces -j ACCEPT -i %v -o %v",
-			a.Name(), b.Name()))
-	} else {
-		return rules.Rule(fmt.Sprintf(
-			"-t filter -A fw-interfaces -j ACCEPT -d %v -i %v -o %v",
-			dst, a.Name(), b.Name()))
-	}
+func forward(a, b Link) rules.Rule {
+	return rules.Rule(fmt.Sprintf(
+		"-t filter -A fw-interfaces -j ACCEPT -i %v -o %v",
+		a.Name(), b.Name()))
+}
+
+func forwardToSubnet(a, b Link, dst string) rules.Rule {
+	return rules.Rule(fmt.Sprintf(
+		"-t filter -A fw-interfaces -j ACCEPT -d %v -i %v -o %v",
+		dst, a.Name(), b.Name()))
 }
 
 func masquerade(l Link) rules.Rule {
