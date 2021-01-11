1c18b89b3f680fc8ea14972da9a80881c848f68e
Jon Betti <jonbetti@gmail.com>
2021-01-11 00:36:25 +0000

    Don't use Split() in Join()
    This muddies whether or not a Suite can be considered "active" or not. I
    don't think they taught one for this in undergrad... maybe I should have
    gone to grad school?

--- vaddr/join.go
+++ vaddr/join.go
@@ -3,9 +3,8 @@ package vaddr
 func Join(s ...Suite) Suite {
 	var r Suite
 	for _, c := range s {
-		w, a := Split(c)
-		r.Wrappers = append(r.Wrappers, w)
-		r.Actives = append(r.Actives, a)
+		r.Wrappers = append(r.Wrappers, c.Wrappers...)
+		r.Actives = append(r.Actives, c.Actives...)
 	}
 	return r
 }
