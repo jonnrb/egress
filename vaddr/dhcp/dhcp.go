175170e5bf6d0d04d93003235c2320635dd6c221
Jon Betti <jonbetti@gmail.com>
2021-01-13 01:31:44 +0000

    Only tear down IP if it changes
    Also only tear it down non-transiently when it passes the rebind time.

--- vaddr/dhcp/dhcp.go
+++ vaddr/dhcp/dhcp.go
@@ -32,59 +32,83 @@ func (a VAddr) Run(ctx context.Context) error {
 			},
 		},
 		Actives: []vaddr.Active{
-			vaddr.ActiveFunc(a.runWithHWAddr),
+			&vaddrState{addr: &a},
 		},
 	}.Run(ctx)
 }
 
-func (a VAddr) runWithHWAddr(ctx context.Context) error {
+type vaddrState struct {
+	addr        *VAddr
+	curLease    *Lease
+	activeVAddr vaddr.Wrapper
+}
+
+func (s *vaddrState) Run(ctx context.Context) error {
+	defer func() {
+		if s.activeVAddr != nil {
+			s.activeVAddr.Stop()
+		}
+	}()
+
 	for {
-		log.V(2).Infof("Requesting a DHCP lease on %v", a.Link.Name())
+		log.V(2).Infof("Requesting a DHCP lease on %v", s.addr.Link.Name())
 
-		l, err := a.getLease(ctx)
+		l, err := s.getLease(ctx)
 		if err != nil {
 			return err
 		}
 
-		log.V(2).Infof("Got DHCP lease for %v: %+v", a.Link.Name(), l)
+		log.V(2).Infof("Got DHCP lease for %v: %+v", s.addr.Link.Name(), l)
 
-		err = a.holdLease(ctx, l)
+		if l == nil {
+			panic("dhcp: impossible nil lease")
+		}
+		err = s.maybeBind(*l)
+		if err != nil {
+			return fmt.Errorf("dhcp: error rebinding: %w", err)
+		}
+
+		err = s.holdLease(ctx)
 		if err != nil {
 			return err
 		}
 
-		log.V(2).Infof("Lease for %v expired", a.Link.Name())
+		log.V(2).Infof("Lease for %v expired", s.addr.Link.Name())
 	}
 }
 
 // Races requesting a lease with getting an existing lease from the LeaseStore.
-func (a VAddr) getLease(ctx context.Context) (l Lease, err error) {
-	type LeaseGetter func(ctx context.Context) (Lease, error)
-	gs := []LeaseGetter{a.requestLease}
-	if a.LeaseStore != nil {
-		gs = append(gs, a.LeaseStore.Get)
-	}
+func (s *vaddrState) getLease(ctx context.Context) (*Lease, error) {
+	var (
+		requestLeaseChan = make(chan Lease, 1)
+		requestLeaseDone = false
+		leaseStoreChan   = make(chan Lease, 1)
+		leaseStoreDone   = false
+	)
 
 	eg, ctx := errgroup.WithContext(ctx)
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
-	c := make(chan Lease, len(gs))
-	for i := range gs {
-		g := gs[i]
+	eg.Go(func() error {
+		l, err := s.requestLease(ctx)
+		requestLeaseChan <- l
+		return err
+	})
+
+	if s.addr.LeaseStore != nil {
 		eg.Go(func() error {
-			if l, err := g(ctx); err != nil {
-				return err
-			} else {
-				c <- l
-				return nil
-			}
+			l, err := s.addr.LeaseStore.Get(ctx)
+			leaseStoreChan <- l
+			return err
 		})
+	} else {
+		leaseStoreDone = true
 	}
 
-	for range gs {
+	for !requestLeaseDone && !leaseStoreDone {
 		select {
-		case l = <-c:
+		case l := <-requestLeaseChan:
 			// Discard if the lease would have entered the renew phase by now.
 			// This is somewhat sensible to do at its face, but also consider
 			// that getLease() runs immediately after the previous lease, which
@@ -92,25 +116,93 @@ func (a VAddr) getLease(ctx context.Context) (l Lease, err error) {
 			// phase. This avoids the LeaseStore racing a DHCP request in a loop
 			// until the lease actually expires.
 			if l.StartTime.Add(l.RenewAfter).Before(time.Now()) {
+				requestLeaseDone = true
+				continue
+			}
+
+			cancel()
+			eg.Wait()
+
+			select {
+			case newLease := <-requestLeaseChan:
+				renewTime := newLease.StartTime.Add(newLease.RenewAfter)
+				if !renewTime.Before(time.Now()) {
+					// Favor a lease just requested. The server would probably
+					// expect us to use it...
+					l = newLease
+				}
+			default:
+			}
+
+			return &l, nil
+		case l := <-requestLeaseChan:
+			if l.StartTime.Add(l.RenewAfter).Before(time.Now()) {
+				requestLeaseDone = true
 				continue
 			}
 			cancel()
 			eg.Wait()
-			return
+			return &l, nil
+		case <-s.timeToRebind():
+			s.unbind()
+			continue
 		case <-ctx.Done():
-			err = eg.Wait()
-			return
+			return nil, eg.Wait()
 		}
 	}
-	err = fmt.Errorf(
-		"dhcp: could not get unexpired lease; last lease seen: %+v", l)
-	return
+	return nil, fmt.Errorf(
+		"dhcp: could not get unexpired lease; last lease seen: %+v", s.curLease)
 }
 
-func (a VAddr) requestLease(ctx context.Context) (l Lease, err error) {
+func (s *vaddrState) timeToRebind() <-chan time.Time {
+	if s.curLease == nil {
+		return nil
+	}
+	rebindTime := s.curLease.StartTime.Add(s.curLease.RebindAfter)
+	return time.After(time.Until(rebindTime))
+}
+
+func (s *vaddrState) maybeBind(l Lease) error {
+	netwiseSame := s.curLease != nil &&
+		s.curLease.LeasedIP.Equal(l.LeasedIP) &&
+		s.curLease.SubnetMask == l.SubnetMask &&
+		s.curLease.GatewayIP.Equal(l.GatewayIP)
+
+	if netwiseSame &&
+		s.activeVAddr != nil &&
+		s.curLease.StartTime.After(l.StartTime) {
+		return nil
+	}
+
+	return s.bind(l)
+}
+
+func (s *vaddrState) bind(l Lease) error {
+	err := s.unbind()
+	if err != nil {
+		s.curLease = nil
+		s.activeVAddr = nil
+		return err
+	}
+	s.curLease = &l
+	s.activeVAddr = s.vaddrForLease(l)
+	return s.activeVAddr.Start()
+}
+
+func (s *vaddrState) unbind() error {
+	s.curLease = nil
+	if s.activeVAddr == nil {
+		return nil
+	}
+	err := s.activeVAddr.Stop()
+	s.activeVAddr = nil
+	return err
+}
+
+func (s *vaddrState) requestLease(ctx context.Context) (l Lease, err error) {
 	c, err := nclient4.New(
-		a.Link.Name(),
-		nclient4.WithHWAddr(a.HWAddr),
+		s.addr.Link.Name(),
+		nclient4.WithHWAddr(s.addr.HWAddr),
 		nclient4.WithRetry(45),
 		nclient4.WithSummaryLogger())
 	if err != nil {
@@ -136,13 +228,18 @@ func (a VAddr) requestLease(ctx context.Context) (l Lease, err error) {
 
 // Holds the lease and potentially writes the lease to a LeaseStore. Returns nil
 // when the lease should be renewed.
-func (a VAddr) holdLease(ctx context.Context, l Lease) error {
+func (s *vaddrState) holdLease(ctx context.Context) error {
+	if s.curLease == nil {
+		panic("dhcp: can't have nil curLease here")
+	}
+	l := *s.curLease
+
 	ctx, cancel := context.WithCancel(ctx)
 	eg, ctx := errgroup.WithContext(ctx)
 
-	if a.LeaseStore != nil {
+	if s.addr.LeaseStore != nil {
 		eg.Go(func() error {
-			err := a.LeaseStore.Put(ctx, l)
+			err := s.addr.LeaseStore.Put(ctx, l)
 			if err != nil {
 				return fmt.Errorf(
 					"dhcp: error putting lease into lease store: %w", err)
@@ -163,35 +260,26 @@ func (a VAddr) holdLease(ctx context.Context, l Lease) error {
 		}
 	})
 
-	eg.Go(func() error {
-		err := a.applyLease(l).Run(ctx)
-		if err != nil {
-			return fmt.Errorf("dhcp: error applying lease: %w", err)
-		}
-		return nil
-	})
-
 	return eg.Wait()
 }
 
-func (a VAddr) applyLease(l Lease) vaddr.Active {
-	return vaddr.Suite{
+func (s *vaddrState) vaddrForLease(l Lease) vaddr.Wrapper {
+	return &vaddr.CombinedWrappers{
 		Wrappers: []vaddr.Wrapper{
 			&vaddrutil.IP{
-				Link: a.Link,
+				Link: s.addr.Link,
 				Addr: leasedAddr(l),
 			},
 			&vaddrutil.DefaultRoute{
-				Link: a.Link,
+				Link: s.addr.Link,
 				GW:   gwAddr(l),
 			},
 			&vaddrutil.GratuitousARP{
-				Link:   a.Link,
-				HWAddr: a.HWAddr,
+				Link:   s.addr.Link,
+				HWAddr: s.addr.HWAddr,
 				IP:     l.LeasedIP,
 			},
 		},
-		Actives: []vaddr.Active{vaddr.ActiveWaiter{}},
 	}
 }
 
--- vaddr/split.go
+++ vaddr/split.go
@@ -9,8 +9,8 @@ import (
 // Splits a Suite into a Wrapper that starts and stops all of the constituent
 // wrappers in order and an Active that concurrently runs all of the constituent
 // actives.
-func Split(s Suite) (Wrapper, Active) {
-	return &suiteSplitWrappers{Suite: s}, suiteSplitActives(s)
+func Split(s Suite) (*CombinedWrappers, CombinedActives) {
+	return &CombinedWrappers{Wrappers: s.Wrappers}, CombinedActives(s.Actives)
 }
 
 func HasActive(s Suite) bool {
@@ -21,13 +21,14 @@ func HasWrapper(s Suite) bool {
 	return len(s.Wrappers) != 0
 }
 
-type suiteSplitWrappers struct {
-	Suite
+type CombinedWrappers struct {
+	Wrappers []Wrapper
+
 	activeWrappers []Wrapper
 	errs           []error
 }
 
-func (s *suiteSplitWrappers) Start() error {
+func (s *CombinedWrappers) Start() error {
 	s.activeWrappers = make([]Wrapper, len(s.Wrappers))[:0]
 	for _, w := range s.Wrappers {
 		err := w.Start()
@@ -41,7 +42,7 @@ func (s *suiteSplitWrappers) Start() error {
 	return nil
 }
 
-func (s *suiteSplitWrappers) Stop() error {
+func (s *CombinedWrappers) Stop() error {
 	for _, w := range reversedWrappers(s.activeWrappers) {
 		err := w.Stop()
 		if err != nil {
@@ -51,15 +52,15 @@ func (s *suiteSplitWrappers) Stop() error {
 	return s.err()
 }
 
-type suiteSplitActives Suite
+type CombinedActives []Active
 
-func (s suiteSplitActives) Run(ctx context.Context) error {
+func (s CombinedActives) Run(ctx context.Context) error {
 	ctx, cancel := context.WithCancel(ctx)
 	eg, ctx := errgroup.WithContext(ctx)
 	defer cancel()
 
-	for i := range s.Actives {
-		a := s.Actives[i]
+	for i := range s {
+		a := s[i]
 		eg.Go(func() error {
 			return a.Run(ctx)
 		})
@@ -68,7 +69,7 @@ func (s suiteSplitActives) Run(ctx context.Context) error {
 	return eg.Wait()
 }
 
-func (s *suiteSplitWrappers) err() error {
+func (s *CombinedWrappers) err() error {
 	// TODO: join.
 	if len(s.errs) != 0 {
 		return s.errs[0]
